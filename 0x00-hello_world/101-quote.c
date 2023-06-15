ee833cc5902cfe43caa05756129f1aba781ed39c
ongaro09 <ongaromark7@gmail.com>
2023-06-15 15:31:31 -0700

    prints using write function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints without using puts function
+ *	or printif function
+ *
+ *	Return: Always 0.
+ *
+ */
+int main(void)
+{
+
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	59);
+
+	return (1);
+
+}
