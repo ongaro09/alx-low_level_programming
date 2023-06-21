5ac7e6b7105dbe4c027b3aa2bbd4ad71ac4fd410
ongaro09 <ongaromark7@gmail.com>
2023-06-20 18:08:03 -0700

    prints putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,3 @@
-/*
- * File: 0-putchar.c
- * Auth: Mark Ongaro
- *
- */
-
 #include "main.h"
 
 /**
@@ -12,15 +6,14 @@
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
