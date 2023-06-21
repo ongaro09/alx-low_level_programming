1d014507d2fa5a70c335edffa589ed4d7d216aff
ongaro09 <ongaromark7@gmail.com>
2023-06-20 18:15:32 -0700

    prints alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Mark Ongaro
+ */
+
+#include "main.h"
+
+/**
+ * this code prints the alphabet in lowercase, followed by a new line.
+ * Return: 0.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
