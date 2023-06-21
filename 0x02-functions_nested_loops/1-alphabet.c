1d032c7606a8d36150f7f72d4fae0121e315b441
ongaro09 <ongaromark7@gmail.com>
2023-06-20 18:31:49 -0700

    prints alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -13,10 +13,10 @@
  */
 void print_alphabet(void)
 {
-        char letter;
-
-        for (letter = 'a'; letter <= 'z'; letter++)
-                _putchar(letter);
-
-        _putchar('\n');
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	
+	_putchar('\n');
 }
