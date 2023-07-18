50d488be7e826ce86de7c219a09ae2afe53478dc
ongaro09 <ongaromark7@gmail.com>
2023-07-18 05:10:01 +0300

    function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,21 +1,54 @@
 #include <stdlib.h>
+#include <string.h>
 #include "dog.h"
 
 /**
- * free_dog - Frees the memory allocated for a dog structure.
- * @d: Pointer to the dog structure.
+ * new_dog - Creates a new dog structure.
+ * @name: Pointer to the name string.
+ * @age: Age of the dog.
+ * @owner: Pointer to the owner string.
+ *
+ * Return: Pointer to the new dog structure.
+ *         NULL if memory allocation fails.
  *
  * Description:
- * This function frees the memory allocated for a dog structure,
- * including the name and owner fields. It ensures that the pointer
- * is not NULL before performing the deallocation.
+ * This function creates a new dog structure and initializes its
+ * name, age, and owner fields by making copies of the provided
+ * name and owner strings. If memory allocation fails, it returns NULL.
  */
-void free_dog(dog_t *d)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d != NULL)
+	dog_t *new_dog;
+	char *name_copy, *owner_copy;
+	int name_len, owner_len;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	name_len = strlen(name);
+	owner_len = strlen(owner);
+
+	name_copy = malloc((name_len + 1) * sizeof(char));
+	if (name_copy == NULL)
 	{
-		free(d->name);
-		free(d->owner);
-		free(d);
+		free(new_dog);
+		return (NULL);
 	}
+	strcpy(name_copy, name);
+
+	owner_copy = malloc((owner_len + 1) * sizeof(char));
+	if (owner_copy == NULL)
+	{
+		free(name_copy);
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(owner_copy, owner);
+
+	new_dog->name = name_copy;
+	new_dog->age = age;
+	new_dog->owner = owner_copy;
+
+	return (new_dog);
 }
