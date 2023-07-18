675f7731fdc34e4512585e7486788db6272d9ce3
ongaro09 <ongaromark7@gmail.com>
2023-07-18 05:07:39 +0300

    function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,45 +1,21 @@
 #include <stdlib.h>
 #include "dog.h"
-#include "stren.c"
-#include "strcpy.c"
+
 /**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- * Return: new dog struct
+ * free_dog - Frees the memory allocated for a dog structure.
+ * @d: Pointer to the dog structure.
+ *
+ * Description:
+ * This function frees the memory allocated for a dog structure,
+ * including the name and owner fields. It ensures that the pointer
+ * is not NULL before performing the deallocation.
  */
-dog_t *new_dog(char *name, float age, char *owner)
+void free_dog(dog_t *d)
 {
-	dog_t *d;
-
-	if (name == NULL || age < 0 || owner == NULL)
-		return (NULL);
-
-	d = malloc(sizeof(dog_t));
-
-	if (d == NULL)
-		return (NULL);
-
-	d->name = malloc(sizeof(char) * (_strlen(name) + 1));
-
-	if (d->name == NULL)
-	{
-		free(d);
-		return (NULL);
-	}
-
-	d->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-
-	if (d->owner == NULL)
+	if (d != NULL)
 	{
 		free(d->name);
+		free(d->owner);
 		free(d);
-		return (NULL);
 	}
-	d->name = _strcpy(d->name, name);
-	d->age = age;
-	d->owner = _strcpy(d->owner, owner);
-
-	return (d);
 }
