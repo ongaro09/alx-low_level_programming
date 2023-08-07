cbe19b4b955d098d25a80619c5e060f31ab011ca
ongaro09 <ongaromark7@gmail.comm>
2023-08-08 01:31:41 +0300

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,24 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
+
+#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#define STDERR_FILENO 2
 #define BUFFER_SIZE 1024
-#define FILE_PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
-
+#define FILE_PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP| S_IROTH)
 #define EXIT_SUCCESS 0
 #define EXIT_CODE_ARGC_ERROR 97
 #define EXIT_CODE_READ_ERROR 98
 #define EXIT_CODE_WRITE_ERROR 99
 #define EXIT_CODE_CLOSE_ERROR 100
-#define EXIT_CODE_PERMISSIONS_ERROR 101
-
 
 int _putchar(char c);
+int main(int argc, char *argv[]);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
