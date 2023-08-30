d1ecf75b01f4a0884dc161f08c24a48e68ee7685
ongaro09 <ongaromark7@gmail.com>
2023-08-30 15:34:18 +0000

    prints a string, followed by a newline

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts_recursion - prints a string followed by a new line.
@@ -21,17 +20,4 @@ void _puts_recursion(char *s)
 	_putchar(*s);
 	_puts_recursion(s + 1);
 
-}
-
-/**
- * main - checks the code
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-	_puts_recursion("C is fun!");
-
-	return (0);
 }
