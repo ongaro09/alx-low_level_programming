551eabc65365a683c4083caa6965550aec3414e1
ongaro09 <ongaromark7@gmail.com>
2023-09-07 22:31:01 +0000

    allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc and
+ *      checks for allocation errors.
+ * @b: Size of memory to allocate.
+ *
+ * Return: Pointer to allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
